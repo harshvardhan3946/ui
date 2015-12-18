7a03d3edeee8aecd603f81ff084ec2bb52964139
hojha <harshvardhanojha@mindagroup.com>
2015-12-18 15:46:33 +0530

    added user location first page

--- js/main.js
+++ js/main.js
@@ -12,7 +12,10 @@
     app.config(['$routeProvider', function ($routeProvider) {
         $routeProvider
             .when("/", {templateUrl: "pages/home.html", controller: "PageCtrl"})
-            .when("/ambulance-services", {templateUrl: "pages/ambulance/ambulance-services.html", controller: "AmbulanceCtrl"})
+            .when("/ambulance-services", {
+                templateUrl: "pages/ambulance/ambulance-services.html",
+                controller: "AmbulanceCtrl"
+            })
             .when("/blood-banks", {templateUrl: "pages/blood-banks.html", controller: "PageCtrl"})
             .when("/doctor-on-call", {templateUrl: "pages/doctor-on-call.html", controller: "PageCtrl"})
             .when("/equipment-on-rent", {templateUrl: "pages/equipment-on-rent.html", controller: "PageCtrl"})
@@ -24,8 +27,16 @@
             .otherwise("/404", {templateUrl: "pages/404.html", controller: "PageCtrl"});
     }]);
 
-    app.controller('PageCtrl', function ($scope) {
-
+    app.controller('PageCtrl', function ($scope, $http) {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(function (position) {
+                $http.get("http://maps.googleapis.com/maps/api/geocode/json?latlng=" + position.coords.latitude + "," + position.coords.longitude)
+                    .success(function (response) {
+                        $scope.userAddress = response.results[1].formatted_address;
+                    }).error(function (error) {
+                        console.log(error);
+                    });
+            });
+        }
     });
-
 })();
